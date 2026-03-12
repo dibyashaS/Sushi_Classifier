5c0a86c029375c7e49c6d92a7f6a4afe90e0f764
Dibyasha Sharma <dibyasha11@gmail.com>
2026-03-12 11:01:37 -0500

    Sushi Classifier First Commit

---
+++ Sushi_Classifier/app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import torch
+import torch.nn as nn
+from torchvision import transforms, models
+from PIL import Image
+
+st.title("🍣🐕 Sushi Classifier")
+
+device = torch.device("cpu")
+
+# Load model
+model = models.resnet18(weights=None)
+model.fc = nn.Linear(model.fc.in_features, 2)
+
+model.load_state_dict(torch.load("best_model.pth", map_location=device))
+model.eval()
+
+class_names = ["Sushi", "Not_Sushi"]
+
+transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor()
+])
+
+uploaded_file = st.file_uploader("Upload an image")
+
+if uploaded_file is not None:
+
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, caption="Uploaded Image")
+
+    img = transform(image).unsqueeze(0)
+
+    with torch.no_grad():
+        outputs = model(img)
+        _, pred = torch.max(outputs,1)
+
+    prediction = class_names[pred.item()]
+
+    st.write("### Prediction:")
+    st.success(prediction)
