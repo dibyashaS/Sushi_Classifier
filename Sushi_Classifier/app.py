ba6e6e600e622b07983a2694f3cf5e8fd3ed56d3
Dibyasha Sharma <dibyasha11@gmail.com>
2026-03-12 11:28:10 -0500

    Update model loading to use dynamic path
    Refactor model path loading to use BASE_DIR.

--- Sushi_Classifier/app.py
+++ Sushi_Classifier/app.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from torchvision import transforms, models
 from PIL import Image
+from pathlib import Path
 
 st.title("🍣🐕 Sushi Classifier")
 
@@ -12,20 +13,22 @@ device = torch.device("cpu")
 model = models.resnet18(weights=None)
 model.fc = nn.Linear(model.fc.in_features, 2)
 
-model.load_state_dict(torch.load("best_model.pth", map_location=device))
+BASE_DIR = Path(__file__).resolve().parent
+MODEL_PATH = BASE_DIR / "best_model.pth"
+
+model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
 model.eval()
 
 class_names = ["Sushi", "Not_Sushi"]
 
 transform = transforms.Compose([
-    transforms.Resize((224,224)),
+    transforms.Resize((224, 224)),
     transforms.ToTensor()
 ])
 
 uploaded_file = st.file_uploader("Upload an image")
 
 if uploaded_file is not None:
-
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image")
 
@@ -33,7 +36,7 @@ if uploaded_file is not None:
 
     with torch.no_grad():
         outputs = model(img)
-        _, pred = torch.max(outputs,1)
+        _, pred = torch.max(outputs, 1)
 
     prediction = class_names[pred.item()]
 
